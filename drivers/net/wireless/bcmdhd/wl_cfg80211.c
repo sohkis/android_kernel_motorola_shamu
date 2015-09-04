b991c53a21fdc086fd81b97c8c10151cacdd4307
engstk <eng.stk@sapo.pt>
2015-09-04 16:36:46 +0100

    Revert "net: wireless: bcmdhd: Added extra debug"
    This reverts commit 60554bcd7a943abf71dab1df79118ac7da04b9b6.
    
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- drivers/net/wireless/bcmdhd/wl_cfg80211.c
+++ drivers/net/wireless/bcmdhd/wl_cfg80211.c
@@ -8167,8 +8167,7 @@ static bool wl_is_linkdown(struct bcm_cfg80211 *cfg, const wl_event_msg_t *e)
 	event == WLC_E_DISASSOC ||
 	event == WLC_E_DEAUTH) {
 #if (WL_DBG_LEVEL > 0)
-	WL_ERR(("Link down Reason : WLC_E_%s reason = %d status = %d\n", wl_dbg_estr[event],
-	    ntoh32(e->reason), ntoh32(e->status)));
+	WL_ERR(("Link down Reason : WLC_E_%s\n", wl_dbg_estr[event]));
 #endif /* (WL_DBG_LEVEL > 0) */
 		return true;
 	} else if (event == WLC_E_LINK) {
